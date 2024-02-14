baea0dead3afd3ed8c5960885bdea410ea342049
kry shitenkova <shitenkovakris@mail.ru>
2024-02-14 17:37:33 +0200

    solve task3

---
+++ task3/task3.cob
@@ -0,0 +1,17 @@
+0100   identification division.
+0200   program-id. ArrayInMax.
+0300   environment division.
+0400   data division.
+0416
+0500   procedure division.
+0600
+0700   program-begin.
+0800       display 'todays message is'.
+0810       perform say-hello.
+0811       display 'i have bounced back'.
+0819
+0900   program-done.
+0910       stop run.
+0911
+0912   say-hello.
+0913       display 'hello'.
