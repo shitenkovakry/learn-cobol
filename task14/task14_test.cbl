c2856ce0e4fe9f64bec9dd7d55cdceb190f52c36
kry shitenkova <shitenkovakris@mail.ru>
2024-02-25 20:28:33 +0200

    add some space between results in array

--- task14/task14_test.cbl
+++ task14/task14_test.cbl
@@ -17,10 +17,10 @@
 0800       move 5 to expected-array-result(3).
 0900
 1000       perform find-odd-in-array.
-1200           display 'actual: ' array-result(1) array-result(2)
-1201            array-result(3).
-1300           display 'expected: ' expected-array-result(1)
-1301            expected-array-result(2) expected-array-result(3).
+1200           display 'actual: ' array-result(1) ' ' array-result(2)
+1201           ' ' array-result(3).
+1300           display 'expected: ' expected-array-result(1) ' '
+1301            expected-array-result(2) ' ' expected-array-result(3).
 1400   stop run.
 1500
 1600   copy 'find-odd-implementation'.
