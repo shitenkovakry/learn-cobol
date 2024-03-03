20df399fe1a7c9b531a17d67a74ef960294162e1
kry shitenkova <shitenkovakris@mail.ru>
2024-03-03 15:26:21 +0200

    find average of array for task16

---
+++ task16/find-average-implementation.cbl
@@ -0,0 +1,4 @@
+010    find-average-of-array.
+020        move 0 to average.
+030
+040        compute average = summa / array-size.
--- task16/task16_test.cbl
+++ task16/task16_test.cbl
@@ -3,7 +3,9 @@
 030    data division.
 040    working-storage section.
 050    copy 'find-summa-variables'.
+051    copy 'find-average-variables'.
 060        01 expected-summa  pic 99.
+061        01 expected-average PIC 9(1)V99.
 070
 080    procedure division.
 081        move 3 to array(6).
@@ -14,10 +16,16 @@
 0400       move 5 to array(1).
 0500
 0600       move 28 to expected-summa.
+0601       move 4.66 to expected-average.
 0900
 1000       perform find-summa-of-array.
 1200           display 'actual summa: ' summa
-1300           display 'expected summa: ' expected-summa
+1300           display 'expected summa: ' expected-summa.
+1301
+1302       perform find-average-of-array.
+1303           display 'actual average: ' average
+1304           display 'expected average: ' expected-average.
 1400   stop run.
 1500
 1600   copy 'find-summa-implementation'.
+1700   copy 'find-average-implementation'.
